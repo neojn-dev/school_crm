b35e95d6895562b65104fee072c8fae4d2529a8e
Gyan <61640241+gyan-sharma@users.noreply.github.com>
2025-08-24 00:51:56 +0530

    migration simplification

--- prisma/migrations/20250823102129_remove_password_field_from_form_library/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `passwordField` on the `FormLibrary` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `FormLibrary` DROP COLUMN `passwordField`;
